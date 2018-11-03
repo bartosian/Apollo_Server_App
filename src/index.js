7009525960db48bbdc79f9a7205f1f6e68a2c0f3
bartosian <jobartosian1989@gmail.com>
2018-11-03 16:02:58 -0400

    Created apollo-server with types

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import 'dotenv/config';
+
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+
+const app = express();
+
+const schema = gql`
+    type Query {
+        me: User
+    }
+    
+    type User {
+        username: String!
+    }
+`;
+const resolvers = '';
+
+const server = new ApolloServer({
+   typeDefs: schema,
+   resolvers
+});
+
+server.applyMiddleware({
+    app,
+    path: '/graphql'
+});
